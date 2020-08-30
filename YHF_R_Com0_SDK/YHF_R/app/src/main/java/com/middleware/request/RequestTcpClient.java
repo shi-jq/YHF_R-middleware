7704dc78ac37ce2148815b02be4388e2be50dcf1
shi-jq <shi_jq@users.noreply.github.com>
2020-08-31 00:21:49 +0800

    tcp server 和tcp client 解析

--- YHF_R_Com0_SDK/YHF_R/app/src/main/java/com/middleware/request/RequestTcpClient.java
+++ YHF_R_Com0_SDK/YHF_R/app/src/main/java/com/middleware/request/RequestTcpClient.java
@@ -1,5 +1,14 @@
 package com.middleware.request;
 
+import com.middleware.frame.common.INT32U;
+import com.middleware.frame.common.PrintCtrl;
+import com.middleware.frame.data.DataProc;
+import com.middleware.frame.data.RFIDFrame;
+import com.middleware.frame.data.RFrame;
+import com.middleware.frame.data.RFrameList;
+import com.middleware.frame.main.FrameMsgObervable;
+import com.middleware.frame.main.MsgMngr;
+
 import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.future.ConnectFuture;
 import org.apache.mina.core.service.IoConnector;
@@ -17,6 +26,10 @@ public class RequestTcpClient extends IoHandlerAdapter implements Observer
     private IoConnector connector;
     private static IoSession session;
 
+    private DataProc mProc = new DataProc();
+    private RFrameList mRFrameList = new RFrameList();
+    private FrameMsgObervable toAndroid = null;
+
     public RequestTcpClient(String ipAddr, int port) {
         connector = new NioSocketConnector();
         connector.setHandler(this);
@@ -24,16 +37,59 @@ public class RequestTcpClient extends IoHandlerAdapter implements Observer
         connFuture.awaitUninterruptibly();
         session = connFuture.getSession();
         System.out.println("TCP 客户端启动");
+
+        MsgMngr.AndroidToPcTagObv.addObserver(this);
+        toAndroid = MsgMngr.PcToAndroidMsgObv;
     }
 
     @Override
     public void messageReceived(IoSession iosession, Object message)
             throws Exception {
         IoBuffer bbuf = (IoBuffer) message;
-        byte[] byten = new byte[bbuf.limit()];
-        bbuf.get(byten, bbuf.position(), bbuf.limit());
-        //System.out.println("客户端收到消息" + ByteAndStr16.Bytes2HexString(byten));
+        byte[] buffer = new byte[bbuf.limit()];
+        int bsize = bbuf.limit();
+        bbuf.get(buffer, bbuf.position(), bsize);
+
+        PrintCtrl.PrintBUffer("数据从PC读取 ", buffer, bsize);
+
+        if (bsize > 0)
+        {
+            mProc.UnPackMsg(buffer,bsize);
+            mProc.GetFrameList(mRFrameList);
+            for (int i = 0; i < mRFrameList.GetCount(); i++) {
+                RFrame pRFrame = mRFrameList.GetRFrame(i);
+                RFIDFrame rfidFrame = new RFIDFrame(pRFrame);
+                toAndroid.dealFrame(rfidFrame);
+                sendResultToPc(iosession,rfidFrame);
+            }
+            mRFrameList.ClearAll();
+        }
     }
+
+
+    private void sendResultToPc(IoSession iosession,RFIDFrame rfidFrame)
+    {
+        assert  (rfidFrame != null);
+
+        RFrame recvFrame = rfidFrame.GetRevFrame();
+        if (recvFrame == null)
+        {
+            //有可能没有应答, 没有应答则不做处理,因为本身已经超时了
+            return;
+        }
+
+        byte[] pForSend = new byte[DataProc.SEND_FRAME_MAXBUFF];
+        INT32U totalFrameSize = new INT32U(DataProc.SEND_FRAME_MAXBUFF);
+
+        mProc.PackMsg(pForSend,totalFrameSize, recvFrame);
+
+        IoBuffer buffer = IoBuffer.allocate(10);
+        buffer.put(pForSend,0,totalFrameSize.GetValue());
+        buffer.flip();
+        iosession.write(buffer);
+        PrintCtrl.PrintBUffer("数据发送到PC ", pForSend, totalFrameSize.GetValue());
+    }
+
     @Override
     public void exceptionCaught(IoSession session, Throwable cause)
             throws Exception {
@@ -69,6 +125,33 @@ public class RequestTcpClient extends IoHandlerAdapter implements Observer
 
     @Override
     public void update(Observable o, Object arg) {
+        //如果已经断开了, 就把数据丢了
+       if (session == null)
+       {
+           return;
+       }
 
+       if (!session.isConnected())
+       {
+           return;
+       }
+
+        if ( MsgMngr.AndroidToPcTagObv == o)
+        {
+            RFrame frame = (RFrame) arg;
+            assert  (frame != null);
+
+            byte[] pForSend = new byte[DataProc.SEND_FRAME_MAXBUFF];
+            INT32U totalFrameSize = new INT32U(DataProc.SEND_FRAME_MAXBUFF);
+
+            mProc.PackMsg(pForSend,totalFrameSize, frame);
+
+            IoBuffer buffer = IoBuffer.allocate(10);
+            buffer.put(pForSend,0,totalFrameSize.GetValue());
+            buffer.flip();
+            session.write(buffer);
+
+            PrintCtrl.PrintBUffer("标签数据发送到PC ", pForSend, totalFrameSize.GetValue());
+        }
     }
 }
--- YHF_R_Com0_SDK/YHF_R/app/src/main/java/com/middleware/request/RequestTcpServer.java
+++ YHF_R_Com0_SDK/YHF_R/app/src/main/java/com/middleware/request/RequestTcpServer.java
@@ -1,5 +1,15 @@
 package com.middleware.request;
 
+import com.middleware.frame.common.INT32U;
+import com.middleware.frame.common.PrintCtrl;
+import com.middleware.frame.data.DataProc;
+import com.middleware.frame.data.RFIDFrame;
+import com.middleware.frame.data.RFrame;
+import com.middleware.frame.data.RFrameList;
+import com.middleware.frame.main.FrameMsgObervable;
+import com.middleware.frame.main.MsgMngr;
+
+import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
@@ -7,54 +17,97 @@ import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Observable;
+import java.util.Observer;
 
-public class RequestTcpServer extends IoHandlerAdapter
+public class RequestTcpServer extends IoHandlerAdapter  implements Observer
 {
+
+    private DataProc mProc = new DataProc();
+    private RFrameList mRFrameList = new RFrameList();
+    private FrameMsgObervable toAndroid = null;
+    private static IoSession readTagSession = null;
+
     public RequestTcpServer(int port) throws IOException {
         NioSocketAcceptor acceptor = new NioSocketAcceptor();
         acceptor.setHandler(this);
         acceptor.bind(new InetSocketAddress(port));
         System.out.println("TCP服务启动，端口：" + port);
+
+        MsgMngr.AndroidToPcTagObv.addObserver(this);
+        toAndroid = MsgMngr.PcToAndroidMsgObv;
     }
 
     @Override
     public void messageReceived(IoSession session, Object message)
             throws Exception
     {
+        IoBuffer bbuf = (IoBuffer) message;
+        byte[] buffer = new byte[bbuf.limit()];
+        int bsize = bbuf.limit();
+        bbuf.get(buffer, bbuf.position(), bsize);
 
-//        IoBuffer bbuf = (IoBuffer) message;
-//        byte[] byten = new byte[bbuf.limit()];
-//        bbuf.get(byten, bbuf.position(), bbuf.limit());
-//        //System.out.println("收到消息：" + ByteAndStr16.Bytes2HexString(byten));
-//        byte[] bts = new byte[10];
-//        for(int i=0;i<10;i++){
-//            bts[i] = (byte)i;
-//        }
+        PrintCtrl.PrintBUffer("数据从PC读取 ", buffer, bsize);
+
+        if (bsize > 0)
+        {
+            mProc.UnPackMsg(buffer,bsize);
+            mProc.GetFrameList(mRFrameList);
+            for (int i = 0; i < mRFrameList.GetCount(); i++) {
+                RFrame pRFrame = mRFrameList.GetRFrame(i);
+                RFIDFrame rfidFrame = new RFIDFrame(pRFrame);
+                toAndroid.dealFrame(rfidFrame);
+                if (mProc.isStartRead( pRFrame.GetRfidCommand()))
+                {
+                    this.readTagSession = session;
+                }
+                sendResultToPc(session,rfidFrame);
+            }
+            mRFrameList.ClearAll();
+        }
+    }
 
 
-//        IoBuffer buffer = IoBuffer.allocate(10);
-//        buffer.put(bts);
-//        buffer.flip();
-//        session.write(buffer);
+    private void sendResultToPc(IoSession iosession,RFIDFrame rfidFrame)
+    {
+        assert  (rfidFrame != null);
 
+        RFrame recvFrame = rfidFrame.GetRevFrame();
+        if (recvFrame == null)
+        {
+            //有可能没有应答, 没有应答则不做处理,因为本身已经超时了
+            return;
+        }
 
-//      // 拿到所有的客户端Session
-//      Collection<IoSession> sessions = session.getService().getManagedSessions().values();
-//      // 向所有客户端发送数据
-//      for (IoSession sess : sessions) {
-//          sess.write(buffer);
-//      }
+        byte[] pForSend = new byte[DataProc.SEND_FRAME_MAXBUFF];
+        INT32U totalFrameSize = new INT32U(DataProc.SEND_FRAME_MAXBUFF);
+
+        mProc.PackMsg(pForSend,totalFrameSize, recvFrame);
+
+        IoBuffer buffer = IoBuffer.allocate(10);
+        buffer.put(pForSend,0,totalFrameSize.GetValue());
+        buffer.flip();
+        iosession.write(buffer);
+        PrintCtrl.PrintBUffer("数据发送到PC ", pForSend, totalFrameSize.GetValue());
     }
 
     @Override
     public void sessionClosed(IoSession session) throws Exception {
         System.out.println("会话关闭");
+        if (this.readTagSession == session )
+        {
+            this.readTagSession = null;
+        }
     }
     @Override
     public void exceptionCaught(IoSession session, Throwable cause)
             throws Exception {
         System.out.println("会话异常");
         super.exceptionCaught(session, cause);
+        if (this.readTagSession == session )
+        {
+            this.readTagSession = null;
+        }
     }
     @Override
     public void messageSent(IoSession iosession, Object obj) throws Exception {
@@ -76,5 +129,30 @@ public class RequestTcpServer extends IoHandlerAdapter
     public void sessionOpened(IoSession iosession) throws Exception {
         System.out.println("会话打开");
         super.sessionOpened(iosession);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if ( MsgMngr.AndroidToPcTagObv == o)
+        {
+            RFrame frame = (RFrame) arg;
+            assert  (frame != null);
+
+            byte[] pForSend = new byte[DataProc.SEND_FRAME_MAXBUFF];
+            INT32U totalFrameSize = new INT32U(DataProc.SEND_FRAME_MAXBUFF);
+
+            mProc.PackMsg(pForSend,totalFrameSize, frame);
+
+            IoBuffer buffer = IoBuffer.allocate(10);
+            buffer.put(pForSend,0,totalFrameSize.GetValue());
+            buffer.flip();
+
+            if (readTagSession != null)
+            {
+                readTagSession.write(buffer);
+            }
+
+            PrintCtrl.PrintBUffer("标签数据发送到PC ", pForSend, totalFrameSize.GetValue());
+        }
     }
 }
