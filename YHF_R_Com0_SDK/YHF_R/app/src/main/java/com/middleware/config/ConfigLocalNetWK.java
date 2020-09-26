3a51c485bb2f192420362c27c3b06621fa10c176
shi-jq <shi_jq@users.noreply.github.com>
2020-09-26 17:09:34 +0800

    增加 ip 设置和 ip查询

--- YHF_R_Com0_SDK/YHF_R/app/src/main/java/com/middleware/config/ConfigLocalNetWK.java
+++ YHF_R_Com0_SDK/YHF_R/app/src/main/java/com/middleware/config/ConfigLocalNetWK.java
@@ -6,6 +6,7 @@ import com.middleware.frame.common.INT8U;
 import com.middleware.frame.data.RFrame;
 import com.middleware.frame.data.Tools;
 import com.rfid_demo.ctrl.Util;
+import com.rfid_demo.service.RFIDCMD;
 
 public class ConfigLocalNetWK  {
     public static final String TCP_PORT_KEY = "ConfigLocalNet_TCP_PORT_KEY";
@@ -19,9 +20,9 @@ public class ConfigLocalNetWK  {
 
     public static final int TCP_PORT_DEFAULT = 7086;
     public static final int UDP_PORT_DEFAULT = 7088;
-    public static final String IP_DEFAULT = "127.0.0.1";
+    public static final String IP_DEFAULT = "192.168.1.168";
     public static final String IP_YANMA_DEFAULT = "255.255.255.0";
-    public static final String IP_WANGGUAN_DEFAULT = "127.0.0.1";
+    public static final String IP_WANGGUAN_DEFAULT = "192.168.1.1";
     public static final boolean ADD_TIME_DEFAULT = true;
     public static final ClientType CLIENT_TYPE_DEFAULT = ClientType.NONE;
     public static final String MAC_DEFAULT = "98-E7-F4-4E-7D-5B";
@@ -41,14 +42,24 @@ public class ConfigLocalNetWK  {
     public String mac = MAC_DEFAULT;//转发的ip
 
     public ConfigLocalNetWK() {
-        this.ipAddr = (String) Util.dtGet(IP_KEY, IP_DEFAULT);
-        this.ipYanma = (String) Util.dtGet(IP_YANMA_KEY, IP_YANMA_DEFAULT);
-        this.ipWangGuan = (String) Util.dtGet(IP_WANGGUAN_KEY, IP_WANGGUAN_DEFAULT);
+
         this.tcp_port = (int) Util.dtGet(TCP_PORT_KEY, TCP_PORT_DEFAULT);
         this.udp_port = (int) Util.dtGet(UDP_PORT_KEY, UDP_PORT_DEFAULT);
         this.isAddTime = (boolean) Util.dtGet(ADD_TIME_KEY, ADD_TIME_DEFAULT);
         this.type = (ClientType) Util.dtGet(CLIENT_TYPE_KEY, CLIENT_TYPE_DEFAULT);
         this.mac = (String) Util.dtGet(MAC_KEY, MAC_DEFAULT);
+
+        this.ipAddr= (String) Util.dtGet(IP_KEY, IP_DEFAULT);
+        this.ipYanma = (String) Util.dtGet(IP_YANMA_KEY, IP_YANMA_DEFAULT);
+        this.ipWangGuan = (String) Util.dtGet(IP_WANGGUAN_KEY, IP_WANGGUAN_DEFAULT);
+        String ipAddr_Real  = RFIDCMD.getIpAddrForInterfaces("eth0");
+        String ipYanma_Real = RFIDCMD.getIpAddrMaskForInterfaces("eth0");
+        String ipWangGuan_Real = RFIDCMD.getGateWay();
+
+        if (ipAddr_Real != ipAddr || ipYanma_Real != ipYanma || ipWangGuan_Real != ipWangGuan)
+        {
+            RFIDCMD.setNetIp(ipAddr,ipYanma,ipWangGuan,ipWangGuan,"0.0.0.0");
+        }
     }
 
     public static boolean configWithRFrame(RFrame pRFrame) {
--- YHF_R_Com0_SDK/YHF_R/app/src/main/java/com/rfid_demo/service/RFIDCMD.java
+++ YHF_R_Com0_SDK/YHF_R/app/src/main/java/com/rfid_demo/service/RFIDCMD.java
@@ -4,26 +4,31 @@ import android.content.Intent;
 
 import com.rfid_demo.main.MyApplication;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import android.os.Handler;
+import java.util.Enumeration;
 
 
-public class RFIDCMD
-{
+public class RFIDCMD {
     // 用于设置本次关机时间，表示 2018/1/1 8:30 关机 int[] timeoff={2014,1,1,8,30};
     //用于设置下次开机时间， 表示 2018/1/1 20:00 开机 int[] timeon ={2014,1,1,20,30};
-        public static void ctrllOnAndOff(int[] timeOn, int[] timeoff)
-        {
-            Intent intent = new Intent("com.mrk.setpoweronoff");
-            intent.putExtra("timeon", timeOn);
-            intent.putExtra("timeoff", timeoff);
-            intent.putExtra("enable", true); //使能开关机功能， 设为 false,则为关闭，
-            MyApplication.contxt.sendBroadcast(intent);
-        }
+    public static void ctrllOnAndOff(int[] timeOn, int[] timeoff) {
+        Intent intent = new Intent("com.mrk.setpoweronoff");
+        intent.putExtra("timeon", timeOn);
+        intent.putExtra("timeoff", timeoff);
+        intent.putExtra("enable", true); //使能开关机功能， 设为 false,则为关闭，
+        MyApplication.contxt.sendBroadcast(intent);
+    }
 
-    public static void reboot()
-    {
+    public static void reboot() {
 
         Intent intent = new Intent("reboot.zysd.now");
         MyApplication.contxt.sendBroadcast(intent);
@@ -34,13 +39,130 @@ public class RFIDCMD
         MyApplication.contxt.sendBroadcast(intent);
     }
 
+    public static void setNetIp(String ip, String netmask, String gateway, String dns1, String dns2) {
+        Intent intent = new Intent("zysj.set.etherent.mode");
+
+        intent.putExtra("mode", "STATIC");
+        //STATIC :静态模式 DHCP动态模式
+
+        intent.putExtra("ipAddress", ip);
+
+        intent.putExtra("gateway", gateway);
+
+        intent.putExtra("netmask", netmask);
+
+        intent.putExtra("dns1", dns1);
+
+        intent.putExtra("dns2", dns2);
+
+        MyApplication.contxt.sendBroadcast(intent);
+    }
+
+    public static String getIpAddrForInterfaces(String interfaceName){
+        try {
+            Enumeration<NetworkInterface> enNetworkInterface = NetworkInterface.getNetworkInterfaces(); //获取本机所有的网络接口
+            while (enNetworkInterface.hasMoreElements()) {  //判断 Enumeration 对象中是否还有数据
+                NetworkInterface networkInterface = enNetworkInterface.nextElement();   //获取 Enumeration 对象中的下一个数据
+                if (!networkInterface.isUp()) { // 判断网口是否在使用
+                    continue;
+                }
+                if (!interfaceName.equals(networkInterface.getDisplayName())) { // 网口名称是否和需要的相同
+                    continue;
+                }
+                Enumeration<InetAddress> enInetAddress = networkInterface.getInetAddresses();   //getInetAddresses 方法返回绑定到该网卡的所有的 IP 地址。
+                while (enInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enInetAddress.nextElement();
+                    if (inetAddress instanceof Inet4Address) {  //判断是否未ipv4
+                        return inetAddress.getHostAddress();
+                    }
+//                    判断未lo时
+//                    if (inetAddress instanceof Inet4Address && !inetAddress.isLoopbackAddress()) {
+//                        return inetAddress.getHostAddress();
+//                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static String getIpAddrMaskForInterfaces(String interfaceName) {
+        try {
+            Enumeration<NetworkInterface> networkInterfaceEnumeration = NetworkInterface.getNetworkInterfaces();    //获取本机所有的网络接口
+            while (networkInterfaceEnumeration.hasMoreElements()) { //判断 Enumeration 对象中是否还有数据
+                NetworkInterface networkInterface = networkInterfaceEnumeration.nextElement(); //获取 Enumeration 对象中的下一个数据
+                if (!networkInterface.isUp() && !interfaceName.equals(networkInterface.getDisplayName())) { //判断网口是否在使用，判断是否时我们获取的网口
+                    continue;
+                }
+
+                for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {    //
+                    if (interfaceAddress.getAddress() instanceof Inet4Address) {    //仅仅处理ipv4
+                        return calcMaskByPrefixLength(interfaceAddress.getNetworkPrefixLength());   //获取掩码位数，通过 calcMaskByPrefixLength 转换为字符串
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static String getGateWay() {
+        String [] arr;
+        try {
+            Process  process = Runtime.getRuntime().exec("ip route list table 0");
+            String data = null;
+            BufferedReader ie = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String string = in.readLine();
+
+            arr = string.split("\\s+");
+            return arr[2];
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    //通过子网掩码的位数计算子网掩码
+    public static String calcMaskByPrefixLength(int length) {
+
+        int mask = 0xffffffff << (32 - length);
+        int partsNum = 4;
+        int bitsOfPart = 8;
+        int maskParts[] = new int[partsNum];
+        int selector = 0x000000ff;
+
+        for (int i = 0; i < maskParts.length; i++) {
+            int pos = maskParts.length - 1 - i;
+            maskParts[pos] = (mask >> (i * bitsOfPart)) & selector;
+        }
+
+        String result = "";
+        result = result + maskParts[0];
+        for (int i = 1; i < maskParts.length; i++) {
+            result = result + "." + maskParts[i];
+        }
+        return result;
+    }
+
+    private static String intIP2StringIP(int ip) {
+        return (ip & 0xFF) + "." +
+                ((ip >> 8) & 0xFF) + "." +
+                ((ip >> 16) & 0xFF) + "." +
+                (ip >> 24 & 0xFF);
+    }
+
+
     private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
     private static SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+
     public static void setTime(long time) {
         try {
             Date date = new Date(time);
             Intent intent = new Intent("ysj.set.system.clock");
-            intent.putExtra("time",dateFormat.format(date)+""+timeFormat.format(date));//时间
+            intent.putExtra("time", dateFormat.format(date) + "" + timeFormat.format(date));//时间
             MyApplication.contxt.sendBroadcast(intent);
         } catch (Exception e) {
             e.printStackTrace();
