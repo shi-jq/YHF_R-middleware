068c821ca07b4f0ddfd9f7af2b040da5ba583b3b
shi-jq <shi_jq@users.noreply.github.com>
2020-09-27 21:44:09 +0800

    增加主动工作模式

--- YHF_R_Com0_SDK/YHF_R/app/src/main/java/com/middleware/config/ConfigUpload.java
+++ YHF_R_Com0_SDK/YHF_R/app/src/main/java/com/middleware/config/ConfigUpload.java
@@ -1,4 +1,5 @@
 package com.middleware.config;
+import com.middleware.frame.data.RFrame;
 import com.rfid_demo.ctrl.Util;
 
 //客户端转发的配置 连接配置
@@ -6,6 +7,22 @@ public class ConfigUpload
 {
     public static final String WORK_MODE_KEY = "ConfigUpload_WORK_MODE_KEY";
     public static final String DATA_PORT_KEY = "ConfigUpload_DATA_PORT_KEY";
+    public static final byte[] AutoCommand = {(byte) 0xAA,0x00, (byte) 0xCC,0x03,0x62,0x01,0x01, (byte) 0xFB,0x44};
+
+    public enum WORK_MODE_TYPE
+    {
+        WORK_MODE_COMMAND(0),
+        WORK_MODE_AUTO(1);
+
+        private final int value;
+        WORK_MODE_TYPE(int value) {
+            this.value = value;
+        }
+        public int GetValue() {
+            return  this.value;
+        }
+    };
+
 
     public enum PORT_TYPE
     {
@@ -19,23 +36,26 @@ public class ConfigUpload
         PORT_TYPE(int value) {
             this.value = value;
         }
-        public byte GetValue() {
-            return (byte) this.value;
+        public int GetValue() {
+            return  this.value;
         }
     };
 
-    public int  workMode =  0;//客户端连接的类型
+    public int  workMode =  WORK_MODE_TYPE.WORK_MODE_COMMAND.GetValue();//客户端连接的类型
     public int  dataPush = PORT_TYPE.PORT_TYPE_RS232.GetValue() ;
+    public RFrame autoComandFrame = null;
 
     public ConfigUpload()
     {
-        this.workMode = (int) Util.dtGet(WORK_MODE_KEY,0);
+        this.workMode = (int) Util.dtGet(WORK_MODE_KEY,WORK_MODE_TYPE.WORK_MODE_COMMAND.GetValue());
         this.dataPush = (int) Util.dtGet(DATA_PORT_KEY,PORT_TYPE.PORT_TYPE_RS232.GetValue());
+        autoComandFrame = new RFrame();
+        autoComandFrame.InitHeadAndData(AutoCommand,AutoCommand.length);
     }
 
     public  static byte[] workModeBytes()
     {
-        int model = (int) Util.dtGet(WORK_MODE_KEY,0);
+        int model = (int) Util.dtGet(WORK_MODE_KEY,WORK_MODE_TYPE.WORK_MODE_COMMAND.GetValue());
         return new byte[]{(byte) model};
     }
 
--- YHF_R_Com0_SDK/YHF_R/app/src/main/java/com/middleware/reader/Reader.java
+++ YHF_R_Com0_SDK/YHF_R/app/src/main/java/com/middleware/reader/Reader.java
@@ -2,6 +2,7 @@ package com.middleware.reader;
 
 import com.middleware.config.ConfigMngr;
 import com.middleware.config.ConfigReaderSerial;
+import com.middleware.config.ConfigUpload;
 import com.middleware.frame.common.BaseThread;
 import com.middleware.frame.common.INT32U;
 import com.middleware.frame.common.PrintCtrl;
@@ -66,6 +67,12 @@ public class Reader extends BaseThread implements Observer   {
 
         MsgMngr.AndroidToModelMsgObv.addObserver(this);
         this.resume();
+
+        if (ConfigMngr.getInstance().upload.workMode
+                == ConfigUpload.WORK_MODE_TYPE.WORK_MODE_AUTO.GetValue())
+        {
+            sendFrameToSer(ConfigMngr.getInstance().upload.autoComandFrame);
+        }
     }
 
     @Override
@@ -74,32 +81,38 @@ public class Reader extends BaseThread implements Observer   {
         super.onDestory();
     }
 
-    //普通命令是
-    @Override
-    public void update(Observable o, Object arg) {
-        RFIDFrame rfidFrame = (RFIDFrame) arg;
-        assert  (rfidFrame != null);
+    public void sendFrameToSer(RFrame sendFrame)
+    {
         OutputStream ouputStream =  mSerialPort.getOutputStream();
         assert(ouputStream != null);
         try {
 
             byte[] pForSend = new byte[DataProc.SEND_FRAME_MAXBUFF];
             INT32U totalFrameSize = new INT32U(DataProc.SEND_FRAME_MAXBUFF);
-            RFrame sendFrame =  rfidFrame.GetSendFrame();
             mProc.PackMsg(pForSend,totalFrameSize, sendFrame);
             ouputStream.write(pForSend,0,totalFrameSize.GetValue());
             PrintCtrl.PrintBUffer("数据发送到模块 ", pForSend, totalFrameSize.GetValue());
 
-            if (mProc.isStartRead(sendFrame.GetRfidCommand()))
-            {
-            }
-            else{
-                //会等待超时
-                RFrame pRFrame =  waitRecvFrame(rfidFrame.GetSendFrame(),3000);
-                rfidFrame.AddRevFrame(pRFrame);
-            }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    //普通命令是
+    @Override
+    public void update(Observable o, Object arg) {
+        RFIDFrame rfidFrame = (RFIDFrame) arg;
+        assert  (rfidFrame != null);
+        RFrame sendFrame =  rfidFrame.GetSendFrame();
+        sendFrameToSer(sendFrame);
+
+        if (mProc.isStartRead(sendFrame.GetRfidCommand()))
+        {
+        }
+        else{
+            //会等待超时
+            RFrame pRFrame =  waitRecvFrame(rfidFrame.GetSendFrame(),3000);
+            rfidFrame.AddRevFrame(pRFrame);
         }
     }
 
